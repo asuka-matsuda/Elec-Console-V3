f8fe7addb98ba53e6c75ed6ca5d74137870acfb0
松田 飛鳥 <a-matsuda@mat-ope.com>
2026-08-21 12:46:35 +0900

    refactor: apply flexbox mixins to inline-flex components

---
+++ find_inline.cjs
@@ -0,0 +1,26 @@
+﻿const fs = require('fs');
+const path = require('path');
+
+function walk(dir, files = []) {
+  const list = fs.readdirSync(dir);
+  for (let file of list) {
+    const filePath = path.join(dir, file);
+    const stat = fs.statSync(filePath);
+    if (stat.isDirectory()) {
+      walk(filePath, files);
+    } else if (file.endsWith('.vue') || file.endsWith('.scss')) {
+      files.push(filePath);
+    }
+  }
+  return files;
+}
+
+const files = walk(path.join(process.cwd(), 'app'));
+const targets = [];
+files.forEach(file => {
+  const content = fs.readFileSync(file, 'utf8');
+  if (content.includes('display: inline-flex')) {
+    targets.push(path.relative(process.cwd(), file));
+  }
+});
+console.log(targets.join('\n'));
