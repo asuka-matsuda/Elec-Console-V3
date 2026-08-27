a262348d3de88d8226d7361295e87a3a6636cdd7
松田 飛鳥 <a-matsuda@mat-ope.com>
2026-08-27 14:41:43 +0900

    refactor: rename PortalEventDateInput to PortalSiteCalendarEventDateInput to follow Vue tightly-coupled component naming conventions

--- app/components/features/PortalSiteCalendar.client.vue
+++ app/components/features/PortalSiteCalendar.client.vue
@@ -201,11 +201,11 @@ const typedCalendarOptions = computed(() => calendarOptions.value as CalendarOpt
         <div class="p-event-form__row">
           <div class="p-event-form__field">
             <label>開始日時</label>
-            <PortalEventDateInput v-model="form.start" :type="form.allDay ? 'date' : 'datetime-local'" required />
+            <PortalSiteCalendarEventDateInput v-model="form.start" :type="form.allDay ? 'date' : 'datetime-local'" required />
           </div>
           <div class="p-event-form__field">
             <label>終了日時</label>
-            <PortalEventDateInput v-model="form.end" :type="form.allDay ? 'date' : 'datetime-local'" />
+            <PortalSiteCalendarEventDateInput v-model="form.end" :type="form.allDay ? 'date' : 'datetime-local'" />
           </div>
         </div>
         
