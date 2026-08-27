e9915283980e42602872a087d7bca6180c50749c
松田 飛鳥 <a-matsuda@mat-ope.com>
2026-08-27 12:22:01 +0900

    chore: add custom error.vue to display full error trace on UI

---
+++ error.vue
@@ -0,0 +1,14 @@
+
+<template>
+  <div style="padding: 20px; color: red;">
+    <h1>Error!</h1>
+    <pre>{{ error }}</pre>
+    <button @click="clearError({ redirect: '/' })">Go Home</button>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  error: Object
+})
+</script>
